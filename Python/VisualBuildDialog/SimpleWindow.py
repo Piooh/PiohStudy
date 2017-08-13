a8bebe6f8bcf2a3bb5d7106d6d6a3fab3e6470db
PIOH-BOOK\pioh <piohsmec@gmail.com>
2017-08-13 16:18:36 +0900

    Build System

--- Python/VisualBuildDialog/SimpleWindow.py
+++ Python/VisualBuildDialog/SimpleWindow.py
@@ -1,39 +1,27 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import (QApplication, QComboBox, QDialog,
+ QDialogButtonBox, QFormLayout, QGridLayout, QGroupBox, QHBoxLayout,
+ QLabel, QLineEdit, QMenu, QMenuBar, QPushButton, QSpinBox, QTextEdit, QVBoxLayout)
 
 
-class App(QMainWindow):
+class Dialog(QDialog):
+    
+    def slot_method(self):
+        print('slot method called.')
 
     def __init__(self):
-        super().__init__()
-        self.title = 'PyQt SimpleWindow test'
-        self.left = 100
-        self.top = 100
-        self.width = 648
-        self.height = 480
-        self.initUI()
+        super(Dialog, self).__init__()
 
+        button = QPushButton("Click")
+        button.clicked.connect(self.slot_method)
 
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        self.statusBar().showMessage('message in Statusbar')
+        mainLayout = QVBoxLayout()
+        mainLayout.addWidget(button)
 
-        button = QPushButton('PyQt5 button', self)
-        button.setToolTip('this is an example button')
-        button.move(100, 70)
-        button.clicked.connect(self.on_click)
-
-        self.show()
-
-    @pyqtSlot()
-    def on_click(self):
-        print('button click!!!')
-
+        self.setLayout(mainLayout)
+        self.setWindowTitle("Button Example")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = App()
+    ex = Dialog()
     sys.exit(app.exec_())
